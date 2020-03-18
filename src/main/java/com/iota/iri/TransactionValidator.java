7f8fd1699914c9a300a6f785256099ae0726bebc
DyrellC <dyrell.chapman@iota.org>
2020-03-17 22:06:17 -0600

    return false if not quickset

--- src/main/java/com/iota/iri/TransactionValidator.java
+++ src/main/java/com/iota/iri/TransactionValidator.java
@@ -424,6 +424,7 @@ public class TransactionValidator {
         try {
             if(!quickSetSolid(transactionViewModel) && !checkSolidityPool.contains(transactionViewModel.getHash())){
                 checkSolidityPool.add(transactionViewModel.getHash());
+                return false;
             }
             return true;
         } catch (Exception e) {
